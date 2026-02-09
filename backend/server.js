9c3ba020ef4216c5fda6db5d974efcaaacb311d5
snehak2701 <snehak.27.2004@gmail.com>
2026-02-09 20:03:21 +0530

    Added backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("TailorPro Backend Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
